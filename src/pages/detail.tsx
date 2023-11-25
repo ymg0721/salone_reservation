a1ab417339177c299b92228d8c241397d91fce82
Momotecchan <yonamasahiro@gmail.com>
2023-11-25 19:28:07 +0900

    作品詳細画面構成

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "@emotion/styled";
 import HomeLink from "../components/homeLink";
 import Footer from "../components/footer";
+import Img from "../img/footer01.png";
+import Img2 from "../img/flower.svg";
 import { menuItems } from "../data/menuItems";
 
 const Detail: React.FC = () => {
@@ -15,10 +17,53 @@ const Detail: React.FC = () => {
   return (
     <Wrapper>
       <HomeLink items={multipleBreadcrumbs} />
+      {/* ↓コンポーネント化予定 */}
+      <ImgStyled src={Img} alt="" />
+      <CardBackground>
+        <ImgWrapper>
+          <ImgStyled2 src={Img2} alt="" />
+          <h3>この作品への想い。</h3>
+        </ImgWrapper>
+        <PStyled>
+          あああああああああああああああああああああああああああああああああああああああ
+        </PStyled>
+      </CardBackground>
       <Footer menuItems={menuItems} />
     </Wrapper>
   );
 };
+
+const ImgWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PStyled = styled.p`
+  padding: 0 20px 20px 20px;
+  word-wrap: break-word;
+  white-space: pre-wrap;
+  text-decoration: underline;
+`;
+
+const ImgStyled = styled.img`
+  display: block;
+  margin: 30px auto;
+  max-width: 100%;
+  height: auto;
+`;
+
+const ImgStyled2 = styled.img`
+  display: block;
+  margin: 10px;
+  max-width: 100%;
+  height: auto;
+`;
+
+const CardBackground = styled.div`
+  background-color: gray;
+  height: auto;
+  margin: 20px 10vw 0;
+`;
 
 const Wrapper = styled.div`
   background: pink;
