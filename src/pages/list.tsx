e9dcfcbfa39b98d5c27b14028aabe8c0e3addff3
Momotecchan <yonamasahiro@gmail.com>
2023-12-01 01:26:17 +0900

    作品一覧画面

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import axios from "axios";
 import HomeLink from "../components/homeLink";
 import VerticalComponent from "../components/vertical";
 import Footer from "../components/footer";
 import { menuItems } from "../data/menuItems";
 import { componentsData } from "../data/componentsData";
+
+interface ListType {
+  id: number;
+  src: string;
+  title: string;
+  date: string;
+}
 
 const List: React.FC = () => {
   // パンくずリスト
@@ -12,7 +20,16 @@ const List: React.FC = () => {
     { label: "ホーム", to: "/home" },
     { label: "作品一覧画面", to: "/list" },
   ];
+  const [data, setData] = useState<ListType[]>([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3001/api/v1/list")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+        console.log(json, "作品のDBに接続できました");
+      });
+  }, []);
   const renderComponents = () => {
     const rows: JSX.Element[][] = [];
     for (let i = 0; i < componentsData.length; i += 2) {
@@ -29,18 +46,35 @@ const List: React.FC = () => {
   return (
     <Wrapper>
       <HomeLink items={multipleBreadcrumbs} />
-      <div style={{ overflowY: "auto", maxHeight: "500px" }}>
-        {renderComponents().map((row, index) => (
+      <div
+        style={{
+          overflowY: "auto",
+          maxHeight: "500px",
+          display: "flex",
+          width: "100%",
+          justifyContent: "spaceEvenly",
+        }}
+      >
+        {data.map((item) => (
           <div
-            key={index}
+            key={item.id}
             style={{
               display: "flex",
               marginBottom: "10vw",
               marginTop: "10vw",
               justifyContent: "space-evenly",
+              flexDirection: "column",
             }}
           >
-            {row}
+            <img
+              src={item.src}
+              alt=""
+              style={{
+                width: "10vw",
+              }}
+            />
+            <h2>{item.title}</h2>
+            <p>{item.date}</p>
           </div>
         ))}
       </div>
