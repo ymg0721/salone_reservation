34b4ca1b1cccafd5e8e06ae6c8afc11ef82baf68
anyappyoshinagamahiro <162930606+anyappyoshinagamahiro@users.noreply.github.com>
2024-09-15 14:20:19 +0900

    環境nuxt化

--- src/data/reservationData.ts
+++
@@ -1,9 +0,0 @@
-export const reservationData = [
-  {
-    id: 1,
-    src: "https://images.unsplash.com/photo-1595126739121-68ab4225f9cf?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
-    title: "string",
-    date: "2024/02/22",
-    detail: "テスト",
-  },
-];
